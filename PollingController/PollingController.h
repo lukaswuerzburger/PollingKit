0ba9f3b60a469fb3f6d3423737df965272341e79
Lukas Würzburger <lukas.wuerzburger@truffls.com>
2020-08-13 09:17:37 +0200

    Initial Commit

---
+++ PollingController/PollingController.h
@@ -0,0 +1,19 @@
+//
+//  PollingController.h
+//  PollingController
+//
+//  Created by Lukas Würzburger on 8/13/20.
+//  Copyright © 2020 Lukas Würzburger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PollingController.
+FOUNDATION_EXPORT double PollingControllerVersionNumber;
+
+//! Project version string for PollingController.
+FOUNDATION_EXPORT const unsigned char PollingControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PollingController/PublicHeader.h>
+
+
